5b66bcb9259ced403149f65aac9a3338f719a32a
alexapps <trim.lexx@gmail.com>
2023-11-27 23:48:15 +0200

    Carousel added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Hero from "../components/Hero/Hero";
+import { Content } from '../components/Content/Content';
+import Carousel from '../components/Carousel/Carousel';
 import Features from "../components/Features/Features";
+import { heroOne, heroTwo, heroThree } from '../data/HeroData';
 
 const Home = () => {
     return <>
     <Hero />
     <Features />
+    <Content {...heroOne} />
+			<Content {...heroTwo} />
+			<Content {...heroThree} />
+            <Carousel />
     </> 
 };
 
